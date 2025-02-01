3038624d0816d36ee02073fcfe76e83c34265498
gagoory7 <gagoory7@gmail.com>
2025-02-01 16:16:01 +0900

    bug-49: wandb 간격 고정

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main(args) :
                 torch.save(Recmodel.state_dict(), weight_file)
                 print(f"Model saved at epoch {epoch+1}")
 
-            if (epoch+1) % args.train.wandb_logging_interval == 0:
+            if (epoch+1) % 10 == 0:
                 print("[TEST]")
                 results = trainer.test()
                 results = {key: value.tolist() for key, value in results.items()}
