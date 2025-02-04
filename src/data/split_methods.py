a0215253af8a2bb7cbe888750e63cace33e8a42b
JihoonRyu00 <harryryujh@naver.com>
2025-02-04 10:45:28 +0000

    bug-66: black formatting

--- src/data/split_methods.py
+++ src/data/split_methods.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import train_test_split, KFold
 from typing import List, Tuple
 
 
-def train_test_split_strategy(grouped: pd.DataFrame, test_size: float = 0.2, random_state: int = 42) -> Tuple[List[Tuple[int, List[int]]], List[Tuple[int, List[int]]]]:
+def train_test_split_strategy(
+    grouped: pd.DataFrame, test_size: float = 0.2, random_state: int = 42
+) -> Tuple[List[Tuple[int, List[int]]], List[Tuple[int, List[int]]]]:
     """기본 Train-Test 분할 방식"""
     train_data = []
     test_data = []
@@ -14,26 +16,31 @@ def train_test_split_strategy(grouped: pd.DataFrame, test_size: float = 0.2, ran
         if len(item_list) <= 1:
             train_data.append((user_id, item_list))
         else:
-            train_part, test_part = train_test_split(item_list, test_size=test_size, random_state=random_state)
+            train_part, test_part = train_test_split(
+                item_list, test_size=test_size, random_state=random_state
+            )
             train_data.append((user_id, train_part))
             test_data.append((user_id, test_part))
 
     return train_data, test_data
 
 
-def leave_one_out(grouped: pd.DataFrame) -> Tuple[List[Tuple[int, List[int]]], List[Tuple[int, List[int]]], ]:
+def leave_one_out(grouped: pd.DataFrame) -> Tuple[
+    List[Tuple[int, List[int]]],
+    List[Tuple[int, List[int]]],
+]:
     """Leave-One-Out 방식"""
     train_data = []
     test_data = []
     cold_idx = []
 
     for idx, row in grouped.iterrows():
-        user_id = row['newUserId']
-        item_list = row['newItemId']
+        user_id = row["newUserId"]
+        item_list = row["newItemId"]
 
         if len(item_list) >= 3:
             if 3 <= len(item_list) <= 15:
-                cold_idx.append(user_id)  
+                cold_idx.append(user_id)
             train_part = item_list[:-1]
             test_part = [item_list[-1]]
             train_data.append((user_id, train_part))
@@ -42,9 +49,14 @@ def leave_one_out(grouped: pd.DataFrame) -> Tuple[List[Tuple[int, List[int]]], L
             train_data.append((user_id, item_list))
 
     return train_data, test_data, cold_idx
-#timestamp 추가 필요
 
-def k_fold(grouped: pd.DataFrame, k: int = 5) -> Tuple[List[Tuple[int, List[int]]], List[Tuple[int, List[int]]]]:
+
+# timestamp 추가 필요
+
+
+def k_fold(
+    grouped: pd.DataFrame, k: int = 5
+) -> Tuple[List[Tuple[int, List[int]]], List[Tuple[int, List[int]]]]:
     """K-Fold 방식으로 데이터 분할"""
     train_data = []
     test_data = []
