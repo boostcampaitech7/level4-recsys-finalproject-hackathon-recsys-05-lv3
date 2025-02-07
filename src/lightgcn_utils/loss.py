6e4854eb79a55c355fa0e896ccc0613018d720ed
JihoonRyu00 <harryryujh@naver.com>
2025-02-07 19:17:48 +0000

    feat-76: black

--- src/lightgcn_utils/loss.py
+++ src/lightgcn_utils/loss.py
@@ -1,28 +1,27 @@
 import torch
 import torch.optim as optim
 
+
 class BPRLoss:
-    def __init__(self,
-                 recmodel,
-                 args):
+    def __init__(self, recmodel, args):
         self.config = args.optimizer
         self.model = recmodel
-        self.weight_decay = self.config.args['weight_decay']
-        self.lr = self.config.args['lr']
-        optimizer_class  = getattr(optim,self.config.type)
+        self.weight_decay = self.config.args["weight_decay"]
+        self.lr = self.config.args["lr"]
+        optimizer_class = getattr(optim, self.config.type)
         self.opt = optimizer_class(self.model.parameters(), lr=self.lr)
         # self.opt = optim.Adam(recmodel.parameters(), lr=self.lr)
 
         self.args = args
 
     def predict(self, users, pos, neg):
-        
+
         if self.args.model == "CLCRec":
             loss, reg_loss = self.model.clc_loss(users, pos, neg)
         else:
             loss, reg_loss = self.model.bpr_loss(users, pos, neg)
-            
-        reg_loss = reg_loss*self.weight_decay
+
+        reg_loss = reg_loss * self.weight_decay
         loss = loss + reg_loss
 
         self.opt.zero_grad()
--- src/models/lightgcn.py
+++ src/models/lightgcn.py
@@ -3,47 +3,52 @@ from src.data.dataloader import BasicDataset
 from torch import nn
 
 
-class BasicModel(nn.Module):    
+class BasicModel(nn.Module):
     def __init__(self):
         super(BasicModel, self).__init__()
-    
+
     def getUsersRating(self, users):
         raise NotImplementedError
 
+
 class LightGCN(BasicModel):
-    def __init__(self, 
-                 config:dict, 
-                 dataset:BasicDataset):
+    def __init__(self, config: dict, dataset: BasicDataset):
         super(LightGCN, self).__init__()
 
         self.config = config.model_args
-        self.dataset : BasicDataset = dataset
+        self.dataset: BasicDataset = dataset
         self.__init_weight()
 
     def __init_weight(self):
-        self.num_users  = self.dataset.n_users
-        self.num_items  = self.dataset.m_items
-        self.latent_dim = self.config['latent_dim_rec']
-        self.n_layers = self.config['n_layers']
-        self.keep_prob = self.config['keep_prob']
-        self.A_split = self.config['A_split']
-        self.ssl_batch_size = self.config['ssl_batch_size']
+        self.num_users = self.dataset.n_users
+        self.num_items = self.dataset.m_items
+        self.latent_dim = self.config["latent_dim_rec"]
+        self.n_layers = self.config["n_layers"]
+        self.keep_prob = self.config["keep_prob"]
+        self.A_split = self.config["A_split"]
+        self.ssl_batch_size = self.config["ssl_batch_size"]
         self.embedding_user = torch.nn.Embedding(
-            num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+            num_embeddings=self.num_users, embedding_dim=self.latent_dim
+        )
         self.embedding_item = torch.nn.Embedding(
-            num_embeddings=self.num_items, embedding_dim=self.latent_dim)
-        if self.config['pretrain'] == 0:
-#             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
-#             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
-#             print('use xavier initilizer')
-# random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
+            num_embeddings=self.num_items, embedding_dim=self.latent_dim
+        )
+        if self.config["pretrain"] == 0:
+            #             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
+            #             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
+            #             print('use xavier initilizer')
+            # random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
             nn.init.normal_(self.embedding_user.weight, std=0.1)
             nn.init.normal_(self.embedding_item.weight, std=0.1)
-            print('use NORMAL distribution initilizer')
+            print("use NORMAL distribution initilizer")
         else:
-            self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
-            self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
-            print('use pretarined data')
+            self.embedding_user.weight.data.copy_(
+                torch.from_numpy(self.config["user_emb"])
+            )
+            self.embedding_item.weight.data.copy_(
+                torch.from_numpy(self.config["item_emb"])
+            )
+            print("use pretarined data")
         self.f = nn.Sigmoid()
         self.Graph = self.dataset.getSparseGraph()
         print(f"LightGCN is already to go (dropout:{self.config['dropout']})")
@@ -55,10 +60,10 @@ class LightGCN(BasicModel):
         random_index = torch.rand(len(values)) + keep_prob
         random_index = random_index.int().bool()
         index = index[random_index]
-        values = values[random_index]/keep_prob
+        values = values[random_index] / keep_prob
         g = torch.sparse.FloatTensor(index.t(), values, size)
         return g
-    
+
     def degree_aware_edge_dropout(self, graph, keep_prob):
         """
         아이템의 차수(Degree)에 따라 Edge Drop 확률을 다르게 적용하는 함수.
@@ -67,22 +72,26 @@ class LightGCN(BasicModel):
         """
         device = graph.device
         # 아이템 차수(Degree) 계산
-        item_degrees = torch.bincount(graph.indices()[1]).to(device)  # 아이템(1) 차수 계산
+        item_degrees = torch.bincount(graph.indices()[1]).to(
+            device
+        )  # 아이템(1) 차수 계산
         max_degree = item_degrees.max()
         # 차수에 따라 Edge Drop 비율 조정 (최소 5% ~ 최대 20%)
         item_drop_probs = 0.05 + (0.2 - 0.05) * (1 - (item_degrees / max_degree))
         # 엣지마다 Drop 확률 적용
-        edge_mask = torch.rand(graph._nnz(), device=device) > item_drop_probs[graph.indices()[1]]
+        edge_mask = (
+            torch.rand(graph._nnz(), device=device)
+            > item_drop_probs[graph.indices()[1]]
+        )
         # 새로운 그래프 생성 (Edge Drop이 반영됨)
-        new_graph = torch.sparse.FloatTensor(graph.indices()[:, edge_mask], 
-                                            graph.values()[edge_mask], 
-                                            graph.size())
+        new_graph = torch.sparse.FloatTensor(
+            graph.indices()[:, edge_mask], graph.values()[edge_mask], graph.size()
+        )
 
         return new_graph
 
-
     def __dropout(self, keep_prob):
-        if self.config['use_ssl']:
+        if self.config["use_ssl"]:
             # Degree-aware Edge Dropout 적용
             if self.A_split:
                 graph = []
@@ -97,27 +106,27 @@ class LightGCN(BasicModel):
                     graph.append(self.__dropout_x(g, keep_prob))
             else:
                 graph = self.__dropout_x(self.Graph, keep_prob)
-                
+
         return graph
-    
+
     def computer(self, dropped=False):
         """
         propagate methods for lightGCN
-        """       
+        """
         users_emb = self.embedding_user.weight
         items_emb = self.embedding_item.weight
         all_emb = torch.cat([users_emb, items_emb])
         #   torch.split(all_emb , [self.num_users, self.num_items])
         embs = [all_emb]
-        if self.config['dropout'] or dropped:
+        if self.config["dropout"] or dropped:
             if self.training:
                 print("droping")
                 g_droped = self.__dropout(self.keep_prob)
             else:
-                g_droped = self.Graph        
+                g_droped = self.Graph
         else:
-            g_droped = self.Graph    
-        
+            g_droped = self.Graph
+
         for layer in range(self.n_layers):
             if self.A_split:
                 temp_emb = []
@@ -129,18 +138,18 @@ class LightGCN(BasicModel):
                 all_emb = torch.sparse.mm(g_droped, all_emb)
             embs.append(all_emb)
         embs = torch.stack(embs, dim=1)
-        #print(embs.size())
+        # print(embs.size())
         light_out = torch.mean(embs, dim=1)
         users, items = torch.split(light_out, [self.num_users, self.num_items])
         return users, items
-    
+
     def getUsersRating(self, users):
         all_users, all_items = self.computer()
         users_emb = all_users[users.long()]
         items_emb = all_items
         rating = self.f(torch.matmul(users_emb, items_emb.t()))
         return rating
-    
+
     def getEmbedding(self, users, pos_items, neg_items):
         all_users, all_items = self.computer()
         users_emb = all_users[users]
@@ -150,10 +159,10 @@ class LightGCN(BasicModel):
         pos_emb_ego = self.embedding_item(pos_items)
         neg_emb_ego = self.embedding_item(neg_items)
         return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
-    
+
     def bpr_loss(self, users, pos, neg):
-        
-        if self.config['use_ssl']:
+
+        if self.config["use_ssl"]:
             # Contrastive Learning을 위한 두 개의 증강된 뷰 생성
             users_1, items_1 = self.computer(dropped=True)
             users_2, items_2 = self.computer(dropped=True)
@@ -163,54 +172,64 @@ class LightGCN(BasicModel):
             neg_emb = items_1[neg]
 
             # Contrastive Loss 계산
-            loss_ssl = self.contrastive_loss(users_1, users_2, batch_size=self.ssl_batch_size) + self.contrastive_loss(items_1, items_2,self.ssl_batch_size)
+            loss_ssl = self.contrastive_loss(
+                users_1, users_2, batch_size=self.ssl_batch_size
+            ) + self.contrastive_loss(items_1, items_2, self.ssl_batch_size)
 
-        else: 
-            (users_emb, pos_emb, neg_emb,
-             userEmb0,  posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        else:
+            (users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0) = (
+                self.getEmbedding(users.long(), pos.long(), neg.long())
+            )
             loss_ssl = 0  # 기존 방식에서는 Contrastive Loss 없음
 
-        reg_loss = (1/2)*(userEmb0.norm(2).pow(2) + 
-                         posEmb0.norm(2).pow(2)  +
-                         negEmb0.norm(2).pow(2))/float(len(users))
-        
+        reg_loss = (
+            (1 / 2)
+            * (
+                userEmb0.norm(2).pow(2)
+                + posEmb0.norm(2).pow(2)
+                + negEmb0.norm(2).pow(2)
+            )
+            / float(len(users))
+        )
+
         pos_scores = torch.mul(users_emb, pos_emb)
         pos_scores = torch.sum(pos_scores, dim=1)
         neg_scores = torch.mul(users_emb, neg_emb)
         neg_scores = torch.sum(neg_scores, dim=1)
-        
+
         loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
-        
-        return loss + self.config['ssl_lambda'] * loss_ssl, reg_loss
-    
+
+        return loss + self.config["ssl_lambda"] * loss_ssl, reg_loss
+
     def contrastive_loss(self, z1, z2, temperature=0.5, batch_size=1024):
-            """
-            Contrastive Loss (InfoNCE Loss)
-            - z1, z2: 두 개의 서로 다른 증강된 그래프에서 얻은 노드 임베딩
-            - temperature: Contrastive Learning에서 사용하는 온도(temperature) 파라미터
-            """
-            f = lambda x: torch.exp(x / temperature)
-            # 같은 노드의 서로 다른 뷰 간 유사도 (Positive Pair)
-            pos_sim = torch.sum(torch.mul(z1, z2), dim=1)
-            # Negative Pair를 메모리 효율적으로 계산
-            num_nodes = z1.shape[0]
-            total_loss = 0.0
-            for i in range(0, num_nodes, batch_size):
-                z1_batch = z1[i:i+batch_size]
-                between_sim = f(torch.matmul(z1_batch, z2.T))  # ✅ 작은 배치 단위로 연산
-                total_loss += torch.sum(torch.log(pos_sim[i:i+batch_size] / torch.sum(between_sim, dim=1)))
+        """
+        Contrastive Loss (InfoNCE Loss)
+        - z1, z2: 두 개의 서로 다른 증강된 그래프에서 얻은 노드 임베딩
+        - temperature: Contrastive Learning에서 사용하는 온도(temperature) 파라미터
+        """
+        f = lambda x: torch.exp(x / temperature)
+        # 같은 노드의 서로 다른 뷰 간 유사도 (Positive Pair)
+        pos_sim = torch.sum(torch.mul(z1, z2), dim=1)
+        # Negative Pair를 메모리 효율적으로 계산
+        num_nodes = z1.shape[0]
+        total_loss = 0.0
+        for i in range(0, num_nodes, batch_size):
+            z1_batch = z1[i : i + batch_size]
+            between_sim = f(torch.matmul(z1_batch, z2.T))  # ✅ 작은 배치 단위로 연산
+            total_loss += torch.sum(
+                torch.log(pos_sim[i : i + batch_size] / torch.sum(between_sim, dim=1))
+            )
 
-            # Contrastive Loss (InfoNCE)
-            return -torch.mean(torch.log(pos_sim / torch.sum(between_sim, dim=1)))
+        # Contrastive Loss (InfoNCE)
+        return -torch.mean(torch.log(pos_sim / torch.sum(between_sim, dim=1)))
 
-       
     def forward(self, users, items):
         # compute embedding
         all_users, all_items = self.computer()
         # print('forward')
-        #all_users, all_items = self.computer()
+        # all_users, all_items = self.computer()
         users_emb = all_users[users]
         items_emb = all_items[items]
         inner_pro = torch.mul(users_emb, items_emb)
-        gamma     = torch.sum(inner_pro, dim=1)
+        gamma = torch.sum(inner_pro, dim=1)
         return gamma
